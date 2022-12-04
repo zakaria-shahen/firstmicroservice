a148002298ca321d8ef0017b4b29e1986c2d8a94
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-04 12:25:38 +0200

    add mapstruct and Spring HATEOAS

--- src/main/java/com/mycompany/firstmicroservice/controller/HomeController.java
+++ src/main/java/com/mycompany/firstmicroservice/controller/HomeController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
-    @GetMapping
+    @GetMapping("/")
     public String get() {
+
         return "Hello, Microservice";
+
     }
+
 }
