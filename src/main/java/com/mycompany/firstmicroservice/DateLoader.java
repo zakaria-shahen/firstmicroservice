41f01fd8e6fbf221d55ad7107c0e034304a1c100
zakaria-shahen <zakariay.shahen@gmail.com>
2022-12-05 23:25:50 +0200

    add spring cloud config client

--- src/main/java/com/mycompany/firstmicroservice/DateLoader.java
+++ src/main/java/com/mycompany/firstmicroservice/DateLoader.java
@@ -3,6 +3,9 @@ package com.mycompany.firstmicroservice;
 import com.mycompany.firstmicroservice.model.License;
 import com.mycompany.firstmicroservice.service.LicenseService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
@@ -13,8 +16,15 @@ public class DateLoader implements ApplicationRunner {
 
     private final LicenseService licenseService;
 
+    private final Logger logger = LoggerFactory.getLogger(DateLoader.class);
+
+    @Value("${test.props}")
+    private String test;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
+
+        logger.info(test);
 
         licenseService.create(License.builder()
                         .licenseId("1dfd")
